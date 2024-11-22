abf0abd43840919b60608d5093e8a0c235a83afb
evicsss <evilavictoria@hotmail.com>
2024-11-22 19:03:38 -0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import Header from './components/header/Header'
+import './globalStyle/globalStyle.scss'
+
+export default function App() {
+  return(
+    <Header />
+  )
+}
---
+++ src/pages/livrosDoados/LivrosDoados.jsx
@@ -0,0 +1,5 @@
+export default function LivrosDoados(){
+    return(
+        <h2>Eu sou o componente de livros doados</h2>
+    )
+}
