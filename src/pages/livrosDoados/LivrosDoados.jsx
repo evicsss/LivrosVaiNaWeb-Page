e001c94750d5ab4f503081d61d9537fd9a03ca83
evicsss <evilavictoria@hotmail.com>
2024-11-27 22:56:07 -0300

    Adicionando nova seção de livros doados

--- src/pages/livrosDoados/LivrosDoados.jsx
+++ src/pages/livrosDoados/LivrosDoados.jsx
@@ -1,5 +1,18 @@
+import capaLivro from '../../assets/Livro.png';
+import s from './livrosDoados.module.scss';
+
 export default function LivrosDoados(){
     return(
-        <h2>Eu sou o componente de livros doados</h2>
+        <section className={s.livrosDoadosSection}>
+            <p>Livros Doados</p>
+
+            <div className={s.livroContainer}>
+                <section><img src={capaLivro} alt="Imagem representando o livro o Protagonista" className={s.capaLivro} />
+                <h2>O Protagonista</h2>
+                <h2>Susanne Andrade</h2>
+                <h2>Gênero: Ficção</h2>
+                </section>
+            </div>
+        </section>
     )
 }
--- src/pages/queroDoar/QueroDoar.jsx
+++ src/pages/queroDoar/QueroDoar.jsx
@@ -1,21 +1,19 @@
-import iconLivro from '../../assets/livroForm.png'
-import s from './queroDoar.module.scss'
+import capaLivro from '../../assets/Livro.png';
+import s from './livrosDoados.module.scss';
 
-export default function QueroDoar(){
-    return(
-        <section className={s.queroDoarSection}>
-            <p>Por favor, preencha o formulário com suas informações e as informações do Livro</p>
-            <form action="">
-                <div>
-                    <img src={iconLivro} alt="Icon representando um livro aberto" />
-                    <h2>Informações do Livro</h2>
+export default function LivrosDoados() {
+    return (
+        <section className={s.livrosDoadosSection}>
+            <p>Livros Doados</p>
+            
+            <div className={s.livroContainer}>
+                <img src={capaLivro} alt="Imagem representando o livro O Protagonista" className={s.capaLivro} />
+                <div className={s.livroInfo}>
+                    <h2>O Protagonista</h2>
+                    <h2>Susanne Andrade</h2>
+                    <h2>Gênero: Ficção</h2>
                 </div>
-                <input type="text" name='' id='' placeholder='Título' />
-                <input type="text" name='' id='' placeholder='Categoria' />
-                <input type="text" name='' id='' placeholder='Autor' />
-                <input type="text" name='' id='' placeholder='Link da Imagem' />
-                <button className={s.button}>Doar</button>
-            </form>
+            </div>
         </section>
-    )
+    );
 }
